76bbded9e30dd919654062739d36d1cd055a0684
Natan Haider <101884983+nAhAid@users.noreply.github.com>
2022-10-12 12:00:44 +0200

    structure A01

--- L01/A01_randomPoem/PoemScript.js
+++ L01/A01_randomPoem/PoemScript.js
@@ -1,2 +1,19 @@
 "use strict";
+var Content;
+(function (Content) {
+    let subjects = ["Gandalf", "Bilbo", "Frodo", "Morgoth", "Sauron", "Sam"];
+    let predicates = ["wohnt", "isst", "raucht", "zerstört", "braucht", "will"];
+    let objects = ["den Ring", "Kuchen", "Stich", "Pfeifenkraut", "Isengart", "Beutelsend"];
+    console.log(subjects);
+    console.log(objects);
+    console.log(predicates);
+    for (let index = subjects.length; index >= 1; index--) {
+        console.log(index);
+        let verseReturn = getVerse(subjects, predicates, objects);
+        console.log(verseReturn);
+    }
+    function getVerse(_subjects, _predicates, _objects) {
+        return ("Alohomora");
+    }
+})(Content || (Content = {}));
 //# sourceMappingURL=PoemScript.js.map
--- L01/A01_randomPoem/PoemScript.ts
+++ L01/A01_randomPoem/PoemScript.ts
@@ -1,3 +1,24 @@
 namespace Content {
+    let subjects: string[] = ["Gandalf", "Bilbo", "Frodo", "Morgoth", "Sauron", "Sam"];
+    let predicates: string[] = ["wohnt", "isst", "raucht", "zerstört", "braucht", "will"];
+    let objects: string[] = ["den Ring", "Kuchen", "Stich", "Pfeifenkraut", "Isengart", "Beutelsend"];
+
+
+
+    console.log(subjects);
+    console.log(objects);
+    console.log(predicates);
+
+    for (let index = subjects.length; index >= 1; index--) {
+        console.log(index);
+        let verseReturn: string = getVerse(subjects, predicates, objects);
+        console.log(verseReturn);
+        
+    }
+
+    function getVerse(_subjects: string[], _predicates: string[], _objects: string[]) {
+        return ("Alohomora");
+    }
+
     
 }
