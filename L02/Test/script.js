f1c2e2d12229eaeb11e53f6fcf62ad550d525062
Natan Haider <101884983+nAhAid@users.noreply.github.com>
2022-10-22 13:33:52 +0200

    hmm

---
+++ L02/Test/script.js
@@ -0,0 +1,7 @@
+"use strict";
+var TestSpace;
+(function (TestSpace) {
+    let number = 1;
+    document.querySelector("#text").addEventListener("click", () => console.log(number));
+})(TestSpace || (TestSpace = {}));
+//# sourceMappingURL=script.js.map
---
+++ L02/Test/script.ts
@@ -0,0 +1,7 @@
+namespace TestSpace {
+    let number: number = 1;
+
+    document.querySelector("#text").addEventListener("click", () => console.log(number));
+
+}
+
