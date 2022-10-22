d2a7e42280ef538f686d3a040e733258a3908732
Natan Haider <101884983+nAhAid@users.noreply.github.com>
2022-10-22 16:16:30 +0200

    blackmail script

--- L02/Test/blackmail.js
+++ L02/Test/blackmail.js
@@ -1,2 +1,33 @@
 "use strict";
+var L02_Blackmailer_Companion;
+(function (L02_Blackmailer_Companion) {
+    let choosenCharacter = "A";
+    window.addEventListener("load", handleLoad);
+    function handleLoad(_event) {
+        let mail = document.querySelector("div#mail");
+        mail.addEventListener("click", placeLetter);
+        document.addEventListener("keydown", chooseCharacter);
+    }
+    function placeLetter(_event) {
+        //console.log(_event);
+        let x = _event.offsetX;
+        let y = _event.offsetY;
+        let mail = _event.target;
+        let letter = document.createElement("span");
+        mail.appendChild(letter);
+        letter.textContent = choosenCharacter;
+        letter.style.left = x + "px";
+        letter.style.top = y + "px";
+        letter.addEventListener("click", deleteLetter);
+    }
+    function chooseCharacter(_event) {
+        //console.log(_event);
+        choosenCharacter = _event.key;
+    }
+    function deleteLetter(_event) {
+        let target = _event.target;
+        let parent = target.parentNode;
+        parent.removeChild(target);
+    }
+})(L02_Blackmailer_Companion || (L02_Blackmailer_Companion = {}));
 //# sourceMappingURL=blackmail.js.map
