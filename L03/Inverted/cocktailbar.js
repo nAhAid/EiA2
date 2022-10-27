dff506a1cd6924736149966c25f4d7e6c349f88a
Natan Haider <101884983+nAhAid@users.noreply.github.com>
2022-10-27 13:02:23 +0200

    L03 finish

--- L03/Inverted/cocktailbar.js
+++ L03/Inverted/cocktailbar.js
@@ -20,15 +20,35 @@ var L03_CocktailBar;
         //let inputs: NodeListOf<HTMLInputElement> = document.querySelectorAll("input");
         //console.log(inputs);
         //console.log(inputs[1]);
+        let order = document.getElementById("order_text");
+        order.innerHTML = "";
+        let total = 0;
+        let totalOrder = document.getElementById("total");
         let formData = new FormData(document.forms[0]);
         //console.log(formData);
         for (let entry of formData) {
-            console.log(entry);
             let item = document.querySelector("[value='" + entry[1] + "']");
-            console.log(item);
             let price = Number(item.getAttribute("price"));
-            console.log(price);
+            if (item.name == undefined && entry[0] != "Amount") {
+                let amount = parseFloat(checkAmount());
+                let cocktailPrice = price * amount;
+                order.innerHTML += item.value + "     " + cocktailPrice + " € <br>";
+                total += cocktailPrice;
+            }
+            else if (entry[0] == "Amount") {
+                console.log(item.value);
+            }
+            else if (item.name != "Amount") {
+                order.innerHTML += item.value + "     " + price + " € <br>";
+                total += price;
+            }
         }
+        console.log(total);
+        totalOrder.innerHTML = "<b>" + total + " €";
+    }
+    function checkAmount() {
+        let amount = document.querySelector("input#amount").value;
+        return (amount);
     }
 })(L03_CocktailBar || (L03_CocktailBar = {}));
 //# sourceMappingURL=cocktailbar.js.map
--- L03/Inverted/cocktailbar.ts
+++ L03/Inverted/cocktailbar.ts
@@ -25,15 +25,44 @@ namespace L03_CocktailBar {
         //console.log(inputs);
         //console.log(inputs[1]);
 
+        let order: HTMLDivElement = <HTMLDivElement>document.getElementById("order_text");
+        order.innerHTML = "";
+        let total: number = 0;
+        let totalOrder: HTMLParagraphElement = <HTMLParagraphElement>document.getElementById("total");
+
+
         let formData: FormData = new FormData(document.forms[0]);
         //console.log(formData);
         for (let entry of formData) {
-            console.log(entry);
             let item: HTMLInputElement = <HTMLInputElement>document.querySelector("[value='" + entry[1] + "']");
-            console.log(item);
             let price: number = Number(item.getAttribute("price"));
-            console.log(price);
+        
+            if (item.name == undefined && entry[0] != "Amount") {
+                let amount: number = parseFloat(checkAmount());
+                let cocktailPrice: number = price * amount;
+                order.innerHTML += item.value + "     " + cocktailPrice + " € <br>";
+                total += cocktailPrice;
+            }
+
+            else if (entry[0] == "Amount") {
+                console.log(item.value);
+            }
+
+            else if (item.name != "Amount") {
+                order.innerHTML += item.value + "     " + price + " € <br>";
+                total += price;
+            }
         }
+
+        console.log(total);
+        totalOrder.innerHTML = "<b>" + total + " €";
+
+    }
+
+    function checkAmount() {
+        let amount: string = (<HTMLInputElement>document.querySelector("input#amount")).value;
+
+        return (amount);
     }
 
 
