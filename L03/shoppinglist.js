a85b39d5905d22b5efff2654e52ab86a9d0e8152
Natan Haider <101884983+nAhAid@users.noreply.github.com>
2022-10-28 11:14:27 +0200

    A03_shoppingList: script problems

--- L03/shoppinglist.js
+++ L03/shoppinglist.js
@@ -10,11 +10,21 @@ var L03_shoppingList;
 (function (L03_shoppingList) {
     window.addEventListener("load", handleLoad);
     let formData = new FormData(document.forms[0]);
+    let inputs = [
+        {
+            product: "Milk",
+            quantity: 1,
+            buyNext: true,
+            comment: "Pack",
+            lastPurchase: "17.10.2022"
+        }
+    ];
     function handleLoad() {
         let input = document.getElementById("input");
         input.addEventListener("change", handleInputChange);
         let display = document.getElementById("display");
         display.addEventListener("change", handleListChange);
+        writeList();
     }
     function handleInputChange(_event) {
         for (let entry of formData) {
@@ -28,6 +38,32 @@ var L03_shoppingList;
         console.log(checked.checked);
     }
     function writeList() {
+        let list = document.querySelector("#uList");
+        list.innerHTML = "";
+        console.log(inputs.indexOf);
+        inputs.forEach((value, index) => {
+            let classes = value.buyNext ? "done" : "open";
+            list.innerHTML += "<li id=\"listElement" + index + "\" class=\"" + classes + "\">" + value.product + ", " + value.quantity + ", " + value.comment + ", " + value.lastPurchase + " <img id=\"removeElement" + index + "\" class=\"trash\" src=\"assets/trash.png\"><img id=\"editElement" + index + "\" class=\"trash\" src=\"assets/edit.png\"></li>";
+            let listElement = document.querySelector("#listElement" + index);
+            listElement.addEventListener("click", handleClick);
+            let removeElement = document.querySelector("#removeElement" + index);
+            //removeElement.addEventListener("click", removeList);
+            let editElement = document.querySelector("#editElement" + index);
+            //editElement.addEventListener("click", editList);
+        });
+    }
+    function handleClick(_event) {
+        console.log(_event.target);
+    }
+    function clickList(toDoIndex) {
+        inputs[toDoIndex].buyNext = !inputs[toDoIndex].buyNext;
+        writeList();
+    }
+    function removeList(_elementIndex) {
+        console.log(_elementIndex);
+    }
+    function editList(_elementIndex) {
+        console.log(_elementIndex);
     }
 })(L03_shoppingList || (L03_shoppingList = {}));
 //# sourceMappingURL=shoppinglist.js.map
--- L03/shoppinglist.ts
+++ L03/shoppinglist.ts
@@ -11,16 +11,37 @@ namespace L03_shoppingList {
 
     let formData: FormData = new FormData(document.forms[0]);
 
-    function handleLoad () {
+    interface Input {
+        product: string;
+        quantity: number;
+        buyNext: boolean;
+        comment: string;
+        lastPurchase: string;
+    }
+
+    let inputs: Input[] = [ 
+        {
+            product: "Milk",
+            quantity: 1,
+            buyNext: true,
+            comment: "Pack",
+            lastPurchase: "17.10.2022"
+        }
+
+    ];
+
+
+    function handleLoad() {
         let input: HTMLDivElement = <HTMLDivElement>document.getElementById("input");
         input.addEventListener("change", handleInputChange);
 
         let display: HTMLDivElement = <HTMLDivElement>document.getElementById("display");
         display.addEventListener("change", handleListChange);
+        writeList();
     }
 
-    function handleInputChange (_event: Event) {
-        
+    function handleInputChange(_event: Event) {
+
         for (let entry of formData) {
             //console.log(entry);
             let item: HTMLInputElement = <HTMLInputElement>document.querySelector("[name='" + entry[0] + "']");
@@ -28,12 +49,53 @@ namespace L03_shoppingList {
         }
     }
 
-    function handleListChange (_event: Event) {
+    function handleListChange(_event: Event) {
         let checked: HTMLInputElement = <HTMLInputElement>_event.target;
         console.log(checked.checked);
     }
 
-    function writeList (): void {
-        
+
+
+    function writeList(): void {
+        let list: HTMLElement = <HTMLElement>document.querySelector("#uList");
+        list.innerHTML = "";
+        console.log(inputs.indexOf);
+
+        inputs.forEach((value, index) => {
+            let classes: string = value.buyNext ? "done" : "open";
+            list.innerHTML += "<li id=\"listElement" + index + "\" class=\"" + classes + "\">" + value.product + ", " + value.quantity + ", " + value.comment + ", " + value.lastPurchase + " <img id=\"removeElement" + index + "\" class=\"trash\" src=\"assets/trash.png\"><img id=\"editElement" + index + "\" class=\"trash\" src=\"assets/edit.png\"></li>";
+            let listElement: HTMLElement = <HTMLElement>document.querySelector("#listElement" + index);
+            listElement.addEventListener("click", handleClick);
+
+            let removeElement: HTMLElement = <HTMLElement>document.querySelector("#removeElement" + index);
+            //removeElement.addEventListener("click", removeList);
+            let editElement: HTMLElement = <HTMLElement>document.querySelector("#editElement" + index);
+            //editElement.addEventListener("click", editList);
+
+
+        });
+
+
     }
+
+    function handleClick (_event: MouseEvent): void {
+        console.log(_event.target);
+    }
+
+    function clickList(toDoIndex: number): void {
+
+        inputs[toDoIndex].buyNext = !inputs[toDoIndex].buyNext;
+        writeList();
+    }
+
+    function removeList(_elementIndex: number) {
+        console.log(_elementIndex);
+    }
+
+    function editList(_elementIndex: number) {
+        console.log(_elementIndex);
+    }
+
+    
+
 }
