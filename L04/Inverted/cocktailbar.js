45766d8884f34cbf38f6dbeb0ea461996c97d8e4
Natan Haider <101884983+nAhAid@users.noreply.github.com>
2022-10-29 19:57:06 +0200

    L04: Inverted Task Finish

---
+++ L04/Inverted/cocktailbar.js
@@ -0,0 +1,60 @@
+"use strict";
+var L04_CocktailBar;
+(function (L04_CocktailBar) {
+    window.addEventListener("load", handleLoad);
+    function handleLoad() {
+        L04_CocktailBar.generateContent(L04_CocktailBar.data);
+        let form = document.getElementById("form");
+        form.addEventListener("change", handleChange);
+        let slider = document.querySelector("#amount");
+        slider.addEventListener("input", displayAmount);
+    }
+    function handleChange(_event) {
+        displayOrder();
+    }
+    function displayAmount(_event) {
+        let progress = document.querySelector("progress");
+        let amount = _event.target.value;
+        progress.value = parseFloat(amount);
+    }
+    function displayOrder() {
+        //let inputs: NodeListOf<HTMLInputElement> = document.querySelectorAll("input");
+        //console.log(inputs);
+        //console.log(inputs[1]);
+        let order = document.getElementById("order_text");
+        order.innerHTML = "";
+        let total = 0;
+        let totalOrder = document.getElementById("total");
+        let formData = new FormData(document.forms[0]);
+        //console.log(formData);
+        for (let entry of formData) {
+            let item = document.querySelector("[value='" + entry[1] + "']");
+            let price = Number(item.getAttribute("price"));
+            if (item.name == undefined && entry[0] != "Amount") {
+                let amount = parseFloat(checkAmount());
+                let cocktailPrice = price * amount;
+                order.innerHTML += item.value + "     " + cocktailPrice + " € <br>";
+                total += cocktailPrice;
+            }
+            else if (entry[0] == "Amount") {
+                console.log(item.value);
+            }
+            else if (item.name != "Amount") {
+                order.innerHTML += item.value + "     " + price + " € <br>";
+                total += price;
+            }
+        }
+        console.log(total);
+        total = round(total, 2);
+        console.log(total);
+        totalOrder.innerHTML = "<b>" + total + " €";
+    }
+    function checkAmount() {
+        let amount = document.querySelector("input#amount").value;
+        return (amount);
+    }
+    function round(num, fractionDigits) {
+        return Number(num.toFixed(fractionDigits));
+    }
+})(L04_CocktailBar || (L04_CocktailBar = {}));
+//# sourceMappingURL=cocktailbar.js.map
---
+++ L04/Inverted/cocktailbar.ts
@@ -0,0 +1,77 @@
+namespace L04_CocktailBar {
+    window.addEventListener("load", handleLoad);
+
+    function handleLoad() {
+       
+        generateContent(data);
+
+        let form: HTMLElement = <HTMLElement>document.getElementById("form");
+        form.addEventListener("change", handleChange);
+
+        let slider: HTMLElement = <HTMLElement>document.querySelector("#amount");
+        slider.addEventListener("input", displayAmount);
+    }
+
+    function handleChange(_event: Event): void {
+        displayOrder();
+    }
+
+    function displayAmount(_event: Event): void {
+        let progress: HTMLProgressElement = <HTMLProgressElement>document.querySelector("progress");
+        let amount: string = (<HTMLInputElement>_event.target).value;
+        progress.value = parseFloat(amount);
+
+    }
+
+    function displayOrder() {
+        //let inputs: NodeListOf<HTMLInputElement> = document.querySelectorAll("input");
+        //console.log(inputs);
+        //console.log(inputs[1]);
+
+        let order: HTMLDivElement = <HTMLDivElement>document.getElementById("order_text");
+        order.innerHTML = "";
+        let total: number = 0;
+        let totalOrder: HTMLParagraphElement = <HTMLParagraphElement>document.getElementById("total");
+
+
+        let formData: FormData = new FormData(document.forms[0]);
+        //console.log(formData);
+        for (let entry of formData) {
+            let item: HTMLInputElement = <HTMLInputElement>document.querySelector("[value='" + entry[1] + "']");
+            let price: number = Number(item.getAttribute("price"));
+        
+            if (item.name == undefined && entry[0] != "Amount") {
+                let amount: number = parseFloat(checkAmount());
+                let cocktailPrice: number = price * amount;
+                order.innerHTML += item.value + "     " + cocktailPrice + " € <br>";
+                total += cocktailPrice;
+            }
+
+            else if (entry[0] == "Amount") {
+                console.log(item.value);
+            }
+
+            else if (item.name != "Amount") {
+                order.innerHTML += item.value + "     " + price + " € <br>";
+                total += price;
+            }
+        }
+
+        console.log(total);
+        total = round(total, 2);
+        console.log(total);
+        totalOrder.innerHTML = "<b>" + total + " €";
+
+    }
+
+    function checkAmount() {
+        let amount: string = (<HTMLInputElement>document.querySelector("input#amount")).value;
+
+        return (amount);
+    }
+
+    function round(num: number, fractionDigits: number): number {
+        return Number(num.toFixed(fractionDigits));
+    }
+
+}
